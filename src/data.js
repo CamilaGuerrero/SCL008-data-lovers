4c39aef454ba54b955bd4c5cae131acc4bee2cec
CamilaGuerrero <cguerreroverdejo@gmail.com>
2019-02-26 09:12:18 -0300

    añadiendo window a cada funcion para que los test vean las funciones

--- src/data.js
+++ src/data.js
@@ -2,9 +2,23 @@
 
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
-
-const example = () => {
-  return 'example';
+/*
+aqui van las funciones
+function filterData (data, condition) en archivo src/data.js filtra
+sortData(data, sortBy, sortOrder) en archivo src/data.js ordena
+computeStats(data) en archivo src/data.js esto permite hacer cálculos estadísticos básicos
+*/
+const filterData = () => {
+  // return 'example';
 };
 
-window.example = example;
+const sortData = () => {
+  
+}
+const computeStats = () => {
+
+}
+
+window.filterData = filterData;
+window.sortData = sortData;
+window.computeStats = computeStats;
