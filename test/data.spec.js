06009b920e5356d18c517240a15b546c1d77b18a
CamilaGuerrero <cguerreroverdejo@gmail.com>
2019-02-26 08:29:10 -0300

    maquetando primeros test

--- test/data.spec.js
+++ test/data.spec.js
@@ -4,13 +4,39 @@ require('../src/data');
 require('./data.spec.js');
 
 
-describe('example', () => {
+describe('filter', () => {
   
   it('debería ser una función', () => {
-    assert.equal(typeof example, 'function');
+    assert.equal(typeof filterData, 'function');
   });
+ /* aca debo hacer un test que filtre y seria algo asi 
+ it('deberia retornar xxx para xxx y xxxx', () =>{
+   assert.equal(filterData(data que en este caso sería WORLDBANK, condition),resultado)
+ })
+ */
+})
 
-  it('debería retornar "example"', () => {
-    assert.equal(example(), 'example');
+describe('sort', () => {
+
+  it('debería ser una función', () => {
+    assert.equal(typeof sortData, 'function');
   });
+  /* aca debo hacer un test que ordene y seria algo asi 
+ it('deberia retornar xxx para xxx, xxx y xxxx', () =>{
+   assert.equal(sortData(data que en este caso sería WORLDBANK, sortBy,sortOrder),resultado)
+ })
+ */
+})
+
+describe('',()=>{
+  it('debería ser una función', () => {
+    assert.equal(typeof computeStats, 'function');
+  });
+   /* aca debo hacer un test que haga un calculo estadistico y seria algo asi 
+ it('deberia retornar xxx para xxx que en este caso es la data', () =>{
+   assert.equal(computeStats(data que en este caso sería WORLDBANK, no se si aqui irá el cálculo 
+    que le inserto a data, esto lo debo preguntar),resultado)
+ })
+ */
+
 })
