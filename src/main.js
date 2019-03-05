884e9faee8579456f4b1c53d3468cae6d5b69469
agrvfa <nevinnostt@gmail.com>
2019-03-05 09:04:01 -0300

    funciones del dom

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
 /* Manejo del DOM */
+
+window.onload = () => {
+    document.getElementById("page1").style.display="block";
+   
+    document.getElementById("page2").style.display ="none";
+   
+    document.getElementById("page3").style.display="none";
+   
+   }
+   
+   document.getElementById("pagina2").addEventListener("click",() => {
+   
+    document.getElementById("page1").style.display="none";
+   
+    document.getElementById("page2").style.display ="block";
+   
+    document.getElementById("page3").style.display="none";
+   
+   });
+   
+   document.getElementById("pagina3").addEventListener("click",() => {
+   
+    document.getElementById("page1").style.display="none";
+   
+    document.getElementById("page2").style.display ="none";
+   
+    document.getElementById("page3").style.display="block";
+   
+   });
